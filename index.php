ae02cf729d86295a1ff6beeadec34a7d54973f04
Baki Goxhaj <banago@gmail.com>
2013-04-16 12:12:03 +0200

    Removed some Testes

--- index.php
+++ index.php
@@ -24,6 +24,23 @@
 		<?php endwhile; ?>
 			
 			<?php the_pagination(); ?>
+	
+			<?php //echo paginate_links() ?>
+			
+<div class="pagination">			
+<?php
+global $wp_query;
+
+$big = 999999999; // need an unlikely integer
+
+echo paginate_links( array(
+	'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+	'format' => '?paged=%#%',
+	'current' => max( 1, get_query_var('paged') ),
+	'total' => $wp_query->max_num_pages
+) );
+?></div>
+
 				
 		<?php else : ?>
 
--- sidebar.php
+++ sidebar.php
@@ -2,10 +2,5 @@
 <aside class="sidebar" role="complementary">
 
 	<?php dynamic_sidebar(); ?>
-	
-	<div class="widget recent-comments">
-		<h3>Recent Comments</h3>	
-		<?php bg_recent_comments(); ?>		
-	</div>
-	
+
 </aside>
